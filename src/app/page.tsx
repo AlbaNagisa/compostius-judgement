82d67dd8973d2c26252faca8b1dd832797be12e9
ThomasFzr <thfltzr@gmail.com>
2025-04-22 16:28:57 +0200

    Refactor page layout for improved readability and visual hierarchy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,23 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div className="flex flex-col w-full h-screen items-center justify-center">
-      <span className="text-5xl font-bold">Purifie ton karma</span>
+      <span 
+        className="text-8xl font-bold bg-gradient-to-b from-white to-gray-600 bg-clip-text text-transparent"
+      >
+        Purifie ton karma
+      </span>
 
       <div className="flex flex-row items-center justify-center font-[Inter]">
-        <div className="flex flex-col items-center justify-center w-[35%]">
-          <div className="text-2xl pb-4">
-            Es-tu un gaspilleur sans scrupule… ou un maître du zéro déchet ?
-          </div>
+        <div className="flex flex-col items-center justify-center w-[33%]">
+            <div className="text-2xl pb-4 font-[Cinzel] font-bold">
+            ES-TU UN GASPILLEUR SANS SCRUPULE… OU UN MAÎTRE <span style={{ color: "#4CAF50" }}>DU ZÉRO DÉCHET ?</span>
+            </div>
           <div>Bienvenue dans Le Jugement de Compostius, le test de pureté éco-responsable qui scanne ton âme... et ton bac à légumes.</div>
         </div>
 
         <img className="w-64" src='logo.png' alt='Description of the image'></img>
 
-        <div className="flex flex-col items-center justify-center w-[35%]">
+        <div className="flex flex-col items-center justify-center w-[33%]">
           <div className="pb-4">À travers un quizz narratif interactif, découvre ton niveau de karma écologique et affronte tes pires habitudes alimentaires.</div>
           <div className="pb-4">🥦 Réponds à des questions inspirées de la vie réelle (et du fond de ton frigo).</div>
           <div className="pb-4"> 🎭 Chaque choix modifie ton score de pureté et déclenche une réaction divine de Compostius.</div>
@@ -34,6 +38,6 @@ export default function Page() {
           COMMENCER LE TEST
         </Link>
       </button>
-    </div>
+    </div >
   );
 }
